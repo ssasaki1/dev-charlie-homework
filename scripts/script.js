a1ac080fd9716e92095073bd93d73ec5a905fa33
ssasaki <150175118+ssasaki1@users.noreply.github.com>
2024-10-22 11:20:41 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+$('body').css("background-color", "yellow");
+
+// when button clicked
+
+$("btnGetAnimal").on("click",function(){
+
+   //get the user name
+  let bakerName = $("bakerName").val();
+
+   $("greetingArea").text.("Hi there"+ bakerName+"l");
+   //give thema personalized greeting
+})
+  $("greetingArea").after("your spirit animal is: " + randomAnimals)
+
+let spiritAnimals = [,,,,,,,"file not find"]
